fb6512c04c9c3ee47c554fad35d12dbb6f81d8cc
Fabiane Albuquerque <bii.nett@gmail.com>
2019-04-25 20:58:32 -0300

    Refatorando a funcao cpfValidator

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
+cpfValidator = cpf => {
 
-function cpfValidator(cpf) {
-  let Soma = 0;
-  let Resto;
+  let sum = 0;
+  let rest;
  
-  if (cpf.length != 11 || 
-  cpf == "00000000000" || 
-  cpf == "11111111111" || 
-  cpf == "22222222222" || 
-  cpf == "33333333333" || 
-  cpf == "44444444444" || 
-  cpf == "55555555555" || 
-  cpf == "66666666666" || 
-  cpf == "77777777777" || 
-  cpf == "88888888888" || 
-  cpf == "99999999999")
+  if (cpf.length !== 11 || 
+  cpf === "00000000000" || 
+  cpf === "11111111111" || 
+  cpf === "22222222222" || 
+  cpf === "33333333333" || 
+  cpf === "44444444444" || 
+  cpf === "55555555555" || 
+  cpf === "66666666666" || 
+  cpf === "77777777777" || 
+  cpf === "88888888888" || 
+  cpf === "99999999999")
     return false;	
    
-for (i=1; i<=9; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
-Resto = (Soma * 10) % 11;
+for (i=1; i<=9; i++) {
+   sum = sum + parseInt(cpf.substring(i-1, i)) * (11 - i);
+   rest = (sum * 10) % 11;
+} 
+
+  if ((rest === 10) || (rest === 11))  rest = 0;
+  if (rest !== parseInt(cpf.substring(9, 10)) ) return false;
  
-  if ((Resto === 10) || (Resto === 11))  Resto = 0;
-  if (Resto !== parseInt(cpf.substring(9, 10)) ) return false;
- 
-Soma = 0;
-  for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
-  Resto = (Soma * 10) % 11;
- 
-  if ((Resto === 10) || (Resto === 11))  Resto = 0;
-  if (Resto !== parseInt(cpf.substring(10, 11) ) ) return false;
-  return true;
+sum = 0;
+
+  for (i = 1; i <= 10; i++){
+  sum = sum + parseInt(cpf.substring(i-1, i)) * (12 - i);
+  rest = (sum * 10) % 11;
+  }
+
+  if ((rest === 10) || (rest === 11))  rest = 0;
+  if (rest !== parseInt(cpf.substring(10, 11) ) ) return false;
+
+return true;
+
 }
 
 let cpf = "32197584871";
-
 console.log(cpfValidator(cpf));
+
+
