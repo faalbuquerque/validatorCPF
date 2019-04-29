c40eb14fe54896d89594c25da59ba0fe3821b288
Fabiane Albuquerque <bii.nett@gmail.com>
2019-04-29 15:40:51 -0300

    corrigindosemantica da variavel que busca a funcao de cpf

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,45 +1,45 @@
 // executar mocha no terminal com o comando: node node_modules/.bin/mocha
 
 const assert = require('chai').assert;
-const cpfWritten = require('../lib/index');
-const teste = cpfWritten.cpfValidator;
+const cpfSearch = require('../lib/index');
+const cpfTest = cpfSearch.cpfValidator;
 
 
-describe('cpfWritten()', () => {
+describe('Testes da funcao cpfValidator que valida CPFs', () => {
 
   it('CPF em branco deve retornar false', () => {
 	
-    assert.equal(teste(''), false);
+    assert.equal(cpfTest(''), false);
 		
   });
 		
   it('CPF valido deve retornar true', () => {
 	
-    assert.equal(teste('35743443840'), true);
+    assert.equal(cpfTest('35743443840'), true);
 		
   });
 		
   it('CPF invalido deve retornar false', () => {
 	
-    assert.equal(teste('35743443841'), false);
+    assert.equal(cpfTest('35743443841'), false);
 		
   });
 
   it('CPF com espacos em branco deve retornar true', () => {
 	
-    assert.equal(teste('357 434 438 40'), true);
+    assert.equal(cpfTest('357 434 438 40'), true);
 		
   });
     
   it('CPF com ponto e traco deve retornar true', () => {
 	
-    assert.equal(teste('357.434.438-40'), true);
+    assert.equal(cpfTest('357.434.438-40'), true);
 		
   });
     
   it('CPF com letras deve retornar false', () => {
 	
-    assert.equal(teste('35y43a438m40'), false);
+    assert.equal(cpfTest('35y43a438m40'), false);
 		
   }); 
 		
